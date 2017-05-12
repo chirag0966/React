7d643e25c982831709037b9489583b4ec5ee811e
vijayk9643 <vijayk9643@gmail.com>
2017-05-12 12:59:36 +0530

    Review Fixes
    Fix CSS for TodoItem
    
    Add edit/save functionality
    
    add done button to mark task done

--- todo_re_act/src/TodoItem/TodoItem.js
+++ todo_re_act/src/TodoItem/TodoItem.js
@@ -6,7 +6,11 @@ export default class TodoItem extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { isDone: props.isDone };
+		this.state = {
+			isDone: props.isDone,
+			isEditingDisabled: true,
+			editedItemValue: this.props.name
+		};
 
 		this.itemIndex = props.itemIndex;
 	}
@@ -18,17 +22,37 @@ export default class TodoItem extends Component {
 	}
 
 	render() {
-		let isDoneClassName = this.state.isDone ? "listItemDone" : "listItemNotDone";
+		let isDoneClassName = this.state.isDone ? "itemLabel listItemDone" : "itemLabel listItemNotDone";
 		return <li className="myListItem">
-			<div className={isDoneClassName} onClick={this.handleToggleClick}>{this.props.name}</div>
-			<button className="editButton"></button>
-			<button className="deleteButton" onClick={this.deleteItem}></button>
+			<input className={isDoneClassName} type="text" value={this.state.editedItemValue} disabled={this.state.isEditingDisabled}
+				onChange={this.updateEnteredItemValue}></input>
+			{this.state.isEditingDisabled
+				? <button className="actionButton editButton" onClick={this.editItem}></button>
+				: <button className="actionButton saveButton" onClick={this.saveItem}></button>}
+			<button className="actionButton doneTaskButton" onClick={this.handleToggleClick}></button>
+			<button className="actionButton deleteButton" onClick={this.deleteItem}></button>
 		</li>;
 	}
 
 	handleToggleClick = () => {
 		this.setState(prevState => ({
 			isDone: !prevState.isDone
+		}));
+	};
+
+	updateEnteredItemValue = (e) => {
+		this.setState({ editedItemValue: e.target.value });
+	};
+
+	editItem = () => {
+		this.setState(prevState => ({
+			isEditingDisabled: !prevState.isEditingDisabled
+		}));
+	};
+
+	saveItem = () => {
+		this.setState(prevState => ({
+			isEditingDisabled: !prevState.isEditingDisabled
 		}));
 	};
 }
